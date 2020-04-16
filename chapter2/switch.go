d519eb65893da2dc5f1d79a0545701d150158de2
Shin seongsu <tlstjdtn321@naver.com>
2020-04-16 22:37:09 +0900

    first Go Lang

---
+++ chapter2/switch.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := 'a'
+	switch {
+	case '0' <= c && c <= '9':
+		fmt.Printf("%c은(는) 숫자입니다.", c)
+	case 'a' <= c && c <= 'z':
+		fmt.Printf("%c은(는) 소문자 입니다.", c)
+	case 'A' <= c && c <= 'Z':
+		fmt.Printf("%c은(는) 대문자 입니다.", c)
+	}
+}
