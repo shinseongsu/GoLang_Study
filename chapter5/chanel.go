22dd23bb9d6660c95b50244fb7d9fd1158cf80d9
Shin seongsu <tlstjdtn321@naver.com>
2020-04-26 19:14:36 +0900

    고루틴

---
+++ chapter5/chanel.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("main 함수 시작", time.Now())
+
+	done := make(chan bool)
+	go long(done)
+	go short(done)
+
+	<-done
+	<-done
+	fmt.Println("main 함수 종료", time.Now())
+}
+
+func long(done chan bool) {
+	fmt.Println("long 함수 시작", time.Now())
+	time.Sleep(3 * time.Second)
+	fmt.Println("long 함수 종료", time.Now())
+	done <- true
+}
+
+func short(done chan bool) {
+	fmt.Println("short 함수 시작", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("short 함수 종료", time.Now())
+	done <- true
+}
